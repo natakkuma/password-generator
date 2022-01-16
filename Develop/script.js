76b79e0bc9937518d6a3b836e85eae09a0120899
natakkuma <95733427+natakkuma@users.noreply.github.com>
2022-01-16 12:17:54 -0800

    select criteria initial

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,67 @@
 //ASSIGNMENT CODE HERE
 
-/*CONSTANTS & VARIABLES*/
+//VARIABLES
 
-//Character Type Options 
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numeric = "0123456789";
-var special = "!@#$%^&*_-+=";
-
-
-//DOM Elements
-var passwordText = document.getElementById("password");
-var generateBtn = document.getElementById("generate");
+  //Character Type Options 
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numeric = "0123456789";
+  var special = "!@#$%^&*_-+=";
+  
 
 
+  //DOM Elements
+  var passwordText = document.getElementById("password");
+  var generateBtn = document.getElementById("generate");
+
+
+  // Get references to the #generate element
+  var generateBtn = document.querySelector("#generate");
+
+
+//FUNCTIONS
+  //WRITE PASSWORD FUNCTION (to the #password input)
+  function writePassword() {
+
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+  }
+
+  //SELECT CRITERIA FUNCTION
+  function selectCriteria() {
+    //Password Length
+    var passwordLength = window.prompt (
+      "Please enter number of characters for password length. (Minimum length = 8, Maximum length = 128)"
+    );
+
+    //If Pass, proceed to other criteria selection
+    if (8 <= passwordLength && passwordLength <= 128) {
+
+      //lowercase selection
+      var promptLowercase = window.prompt (
+        "Would you like to include lowercase characters? Enter 'YES' or 'NO'."
+      )
+
+      //uppercase selection
+
+    }
+  }
+
+  //GENERATE PASSWORD FUNCTION
+  function generatePassword() {
+
+    //Select Criteria
+    selectCriteria();
+    
+
+  }
 
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+//ACTIONS
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
